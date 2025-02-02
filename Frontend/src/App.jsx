c71a9374268458d6802cbc53049fee0eb7039941
Matthew Cania <matthewgab24@gmail.com>
2025-02-02 11:12:09 +0800

    final commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,17 +5,12 @@ import ProfilePage from "./pages/Profile";
 function App() {
 
   return (
-    <Router>
       <div
         className='flex flex-col justify-start items-center w-auto h-auto bg-zinc-950 overflow-hidden'
       >
-        <Routes>
-
-          <Route path="/" element={<ProfilePage />} />
           {/* <Route path="/test" element={<MouseEffect circleCount={500} circlePx={10} lerp={0.99} interval={2000} color={'black'} />} /> */}
-        </Routes>
+          <ProfilePage/>
       </div>
-    </Router>
   )
 }
 
